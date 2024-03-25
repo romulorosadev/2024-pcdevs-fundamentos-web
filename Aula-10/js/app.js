ea741f64113c6e55a7fb8c2dfadd87d5dea0a7e9
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-24 22:15:38 -0300

    js var in_nome prompt entrada dados

--- Aula-10/js/app.js
+++ Aula-10/js/app.js
@@ -1,1 +1,7 @@
-alert("Teste Aula-04");
+
+// var: Declaracao do tipo de variavel JavaScript. Usada para reservar um espaco na memoria com o objetivo de armazenar e/ou manipular dados
+// in_nome: Nome de identificacao da variavel para armazenar o dado
+// =: Sinal de atribuicao. O codigo/operacao que estiver a direita do sinal de atribuição é armazenado na variavel/elemento declarado a sua esquerda
+// prompt: Metodo JavaScript com a função de exibir uma janela no navegador com o texto passado entre parenteses/parametro do metodo
+//("Digite nome:"): Parametro do metodo/texto a ser exibido pelo prompt
+var in_nome = prompt("Digite nome:")
