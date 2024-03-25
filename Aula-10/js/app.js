2f72fa56358d2b756d7ee709fb39ba730d424de9
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-24 22:16:17 -0300

    js var elem_title para manipular

--- Aula-10/js/app.js
+++ Aula-10/js/app.js
@@ -5,3 +5,10 @@
 // prompt: Metodo JavaScript com a função de exibir uma janela no navegador com o texto passado entre parenteses/parametro do metodo
 //("Digite nome:"): Parametro do metodo/texto a ser exibido pelo prompt
 var in_nome = prompt("Digite nome:")
+
+// var: Declaracao do tipo de variavel JavaScript. Usada para reservar um espaco na memoria com o objetivo de armazenar e/ou manipular dados/tags
+// elem_title: Nome de identificacao da variavel para capturar um elemento na árvore do dom (document object model)
+// =: Sinal de atribuicao. O codigo/operacao que estiver a direita do sinal de atribuição é armazenado na variavel/elemento declarado a sua esquerda
+// document: dom/documento/pagina
+//.getElementById: Metodo JavaScript para capturar no dom um elemento com um atribuito id será passado por parametro (entre parenteses)
+var elem_title = document.getElementById('titulo')
