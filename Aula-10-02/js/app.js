1f07bedaf7184147f84ad72cc8e40e55c146635b
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-25 21:35:46 -0300

    js nomeCompletoParagrafo exibir limpar

--- Aula-10-02/js/app.js
+++ Aula-10-02/js/app.js
@@ -6,6 +6,7 @@ const exibirNomes = () => {
   const elem_sobrenomeInput = document.getElementById("sobrenomeInput");
   const elem_nomeParagrafo = document.getElementById("nomeParagrafo");
   const elem_sobrenomeParagrafo = document.getElementById("sobrenomeParagrafo");
+  const elem_nomeCompletoParagrafo = document.getElementById("nomeCompletoParagrafo");
 
   // VARIAVEL DE ENTRADA
   // Declaradas pra armazenar/manipular o valor digitado no input
@@ -16,10 +17,12 @@ const exibirNomes = () => {
     // Declaradas pra manipular os elementos dos paragrafos
   out_nomeParagrafo = elem_nomeParagrafo;
   out_sobrenomeParagrafo = elem_sobrenomeParagrafo;
+  out_nomeCompletoParagrafo = elem_nomeCompletoParagrafo;
 
   //Insere os texto nos paragrafos referentes para serem exibidos ao usuario
   out_nomeParagrafo.textContent = `NOME: ${in_nome}`;
   out_sobrenomeParagrafo.textContent = `SOBRENOME: ${in_sobrenome}`;
+  out_nomeCompletoParagrafo.textContent = `NOME COMPLETO: ${in_nome} ${in_sobrenome}`;
 }
 
 
@@ -34,4 +37,5 @@ const limparNomes = () => {
   //Limpando o conteudo de texto nos paragrafos para serem exibidos ao usuario
   document.getElementById("nomeParagrafo").textContent = " ";
   document.getElementById("sobrenomeParagrafo").textContent = " ";
+  document.getElementById("nomeCompletoParagrafo").textContent = " ";
 }
