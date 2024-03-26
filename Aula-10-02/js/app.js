e0e2858c3d1d59d73e0087112b25bd924e425cbf
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-25 21:24:26 -0300

    js Botao Limpar

--- Aula-10-02/js/app.js
+++ Aula-10-02/js/app.js
@@ -1,34 +1,37 @@
-// Criada a arrow function exibirNome
-// Ao ser chamada deve percorrer o código presente em seu bloco de código
+// Criada a funcao exibirNome que ao ser chamada percorre seu bloco de código
 const exibirNomes = () => {
 
   //CAPTURANDO TAGS/ELEMENTOS
-  // Com id nomeInput
   const elem_nomeInput = document.getElementById("nomeInput");
-
-  // Com id sobrenomeInput
   const elem_sobrenomeInput = document.getElementById("sobrenomeInput");
-
-  //Com id nomeParagrafo
   const elem_nomeParagrafo = document.getElementById("nomeParagrafo");
-
-  //Com id sobrenomeParagrafo
   const elem_sobrenomeParagrafo = document.getElementById("sobrenomeParagrafo");
 
   // VARIAVEL DE ENTRADA
-  // Declarada pra armazenar/manipular o valor digitado no input
+  // Declaradas pra armazenar/manipular o valor digitado no input
   const in_nome = elem_nomeInput.value;
-
-  // Declarada pra armazenar/manipular o valor digitado no input
   const in_sobrenome = elem_sobrenomeInput.value;
 
   //VARIAVEIS DE SAIDA
-    // Declarada pra manipular o elemento do paragrafo
+    // Declaradas pra manipular os elementos dos paragrafos
   out_nomeParagrafo = elem_nomeParagrafo;
   out_sobrenomeParagrafo = elem_sobrenomeParagrafo;
 
-  //Insere um texto no paragrafo, no caso o valor atribuido a variavel in_nome com uso de template string
+  //Insere os texto nos paragrafos referentes para serem exibidos ao usuario
   out_nomeParagrafo.textContent = `NOME: ${in_nome}`;
   out_sobrenomeParagrafo.textContent = `SOBRENOME: ${in_sobrenome}`;
+}
 
+
+
+// Criada a funcao limparNome que ao ser chamada percorre seu bloco de código
+const limparNomes = () => {
+
+  //Resetando o form e os valores dos inputs.
+  //Volta a exibir o texto de placeholder dos inputs
+  document.getElementById("form").reset();
+
+  //Limpando o conteudo de texto nos paragrafos para serem exibidos ao usuario
+  document.getElementById("nomeParagrafo").textContent = " ";
+  document.getElementById("sobrenomeParagrafo").textContent = " ";
 }
