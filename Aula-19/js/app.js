0a91581f831306a8ad1c81fb4ff86ea112329fdb
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-26 22:34:14 -0300

    app comentarios no código

--- Aula-19/js/app.js
+++ Aula-19/js/app.js
@@ -21,3 +21,22 @@ car.innerHTML = `<p>Marca: <span>${carro.marca}</span></p>
                   <p>Portas: <span>${carro.portas}</span></p>
                   <p>Air Bag: <span>${carro.airbag}</span></p>
                   <p>Ar-Condicionado: <span>${carro.arcondicionado}</span></p>`
+
+// Criar variavel pra capturar o elemento input no dom
+let inputMarca = document.getElementById('inserir');
+
+// É chamado um escutador/addEventListener no input
+// Com isso é disparada uma função toda vez que for inserido um valor no campo
+inputMarca.addEventListener('input', function() {
+
+  //Atribuir o valor inserido no input a propriedade marca do objeto carro
+  carro.marca = this.value;
+
+  //Insere o Template string como código HTML dentro div carro
+  car.innerHTML = `<p>Marca: ${carro.marca}</p>
+                    <p>Preço: ${carro.preco}</p>
+                    <p>Ano: ${carro.ano}</p>
+                    <p>Cor: ${carro.cor}</p>
+                    <p>Motor: ${carro.motor}</p>
+                    <p>Portas: ${carro.portas}</p>`;
+});
