6e19cce97c1239b2c1a82ae0765be3c66ecfc16f
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-29 16:50:02 -0300

    aula-11 manipular dom 3 condicionais

--- Aula-11/js/scripts.js
+++ Aula-11/js/scripts.js
@@ -1,8 +1,14 @@
-var idade = 43;
-
-
 // 1) Manipular o DOM do body - Inserindo um texto
 // document.body.innerText = 'Romulo'
 
 // 2) Manipular o DOM do body - Inserindo HTML
-document.body.innerHTML = `<h2>Rosadev Solutions</h2>`;
+// document.body.innerHTML = `<h2>Rosadev Solutions</h2>`;
+
+// 3) Estrutura Condicional + Manipular o DOM do body inserindo HTML
+const in_idade = 43;
+
+if (in_idade => 18){
+  document.body.innerHTML = '<p>Maior</p>';
+} else {
+  document.body.innerHTML = '<p>mENOR</p>';
+}
