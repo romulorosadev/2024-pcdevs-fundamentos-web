0ecb8587caa5c82647e3b7307d1fab3fafcee376
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-29 16:55:48 -0300

    aula-11 manipular dom 4 prompt condicionais

--- Aula-11/js/scripts.js
+++ Aula-11/js/scripts.js
@@ -5,10 +5,22 @@
 // document.body.innerHTML = `<h2>Rosadev Solutions</h2>`;
 
 // 3) Estrutura Condicional + Manipular o DOM do body inserindo HTML
-const in_idade = 43;
+// const in_idade = 43;
+
+// if (in_idade => 18){
+//   document.body.innerHTML = '<p>Maior</p>';
+// } else {
+//   document.body.innerHTML = '<p>mENOR</p>';
+// }
+
+
+//4) Solicitando dado ao usuário
+const in_idade = parseInt(prompt('Informe sua idade'));
 
 if (in_idade => 18){
-  document.body.innerHTML = '<p>Maior</p>';
+  document.body.innerHTML = `<p>IDADE: ${in_idade}</p>
+                            <p>STATUS: Maior de Idade</p>`;
 } else {
-  document.body.innerHTML = '<p>mENOR</p>';
+  document.body.innerHTML = `<p>IDADE: ${in_idade}</p>
+                            <p>STATUS: Menor de Idade</p>`;
 }
