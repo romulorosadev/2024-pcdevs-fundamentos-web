2dee1d3a89ea873c3feb0763a496d2e2c3d4690e
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-29 16:45:02 -0300

    aula-11 manipular dom 2 innerHTML

--- Aula-11/js/scripts.js
+++ Aula-11/js/scripts.js
@@ -2,4 +2,7 @@ var idade = 43;
 
 
 // 1) Manipular o DOM do body - Inserindo um texto
-document.body.innerText = 'Romulo'
+// document.body.innerText = 'Romulo'
+
+// 2) Manipular o DOM do body - Inserindo HTML
+document.body.innerHTML = `<h2>Rosadev Solutions</h2>`;
