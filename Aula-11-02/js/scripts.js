cff53f409a1b89c756e1e31c5cf764906e36763e
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-29 21:23:06 -0300

    aula-11-02 desafio function verificarAltura

--- Aula-11-02/js/scripts.js
+++ Aula-11-02/js/scripts.js
@@ -6,3 +6,13 @@ const verificarIdade = () => {
   console.log(`IDADE: ${out_idade}`);
   return out_idade;
 }
+
+const verificarAltura = (in_altura) => {
+  const in_altura = parseFloat(document.querySelector('.altura__input').value);
+  const  alturaMinima = 1.75;
+  const  alturaMaxima = 2.10;
+  const  cond_altura  = in_altura >= alturaMinima && in_altura <= alturaMaxima;
+  const out_altura   = cond_altura ? 'Apto': 'Inapto';
+  console.log(`ALTURA: ${out_altura}`);
+  return out_altura;
+}
