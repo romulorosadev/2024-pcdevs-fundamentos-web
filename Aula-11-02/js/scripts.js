716f165ffabb514a4a803115a770854c3cb85ea8
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-30 19:52:59 -0300

    js funcoes exibirInfos() limparInfos()

--- Aula-11-02/js/scripts.js
+++ Aula-11-02/js/scripts.js
@@ -1,30 +1,11 @@
-const verificarIdade = () => {
-  const  in_idade = parseInt(document.querySelector('.idade__input').value);
-  const  idadeMinima = 18;
-  const  cond_idade  = in_idade === idadeMinima;
-  const  out_idade   = cond_idade ? 'Apto': 'Inapto';
-  console.log(`IDADE: ${out_idade}`);
-  return out_idade;
+
+//Criada a função para ser chamada no clique do botao Enviar
+const exibirInfos = () => {
+  console.log("Exibir")
 }
 
-const verificarAltura = (in_altura) => {
-  const in_altura = parseFloat(document.querySelector('.altura__input').value);
-  const  alturaMinima = 1.75;
-  const  alturaMaxima = 2.10;
-  const  cond_altura  = in_altura >= alturaMinima && in_altura <= alturaMaxima;
-  const out_altura   = cond_altura ? 'Apto': 'Inapto';
-  console.log(`ALTURA: ${out_altura}`);
-  return out_altura;
-}
 
-const verificarStatus = () => {
-  verificarIdade();
-  verificarAltura();
-  const out_status = 'Aprovado';
-
-  const condStatus = (verificarIdade === 'Apto' && verificarAltura === 'Apto')
-
-  if(!condRequistos){
-    out_status = 'Reprovado';
-  }
+//Criada a função para ser chamada no clique do botao Limpar
+const limparInfos = () => {
+  console.log("Limpar")
 }
