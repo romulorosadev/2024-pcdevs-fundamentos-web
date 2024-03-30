59e91c6617e3caf1524b1773f61af8dfd1190f7c
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-30 20:04:55 -0300

    js verificacao inputs vazios

--- Aula-11-02/js/scripts.js
+++ Aula-11-02/js/scripts.js
@@ -5,6 +5,16 @@ const exibirInfos = () => {
   //Armazenando os valores inseridos nos inputs já convertidos
   const inIdade = parseInt(document.querySelector('.idade__input').value);
   const inAltura = parseFloat(document.querySelector('.altura__input').value);
+  let outMensagem;
+
+  const verInVazios = isNaN(inIdade) || isNaN(inAltura);
+
+  while(verInVazios){
+    outMensagem = 'Campos vazios. Favor inserir os dados solicitados.'
+    break;
+  }
+
+  console.log(inIdade, inAltura, outMensagem);
 }
 
 
