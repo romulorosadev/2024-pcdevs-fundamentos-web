334f5315f0f6cf0a7e28f73fb3c6bd7ca10439b3
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-03-29 21:22:25 -0300

    aula-11-02 desafio function verificarIdade

--- Aula-11-02/js/scripts.js
+++ Aula-11-02/js/scripts.js
@@ -1,1 +1,8 @@
-alert('Teste Aula-11-02');
+const verificarIdade = () => {
+  const  in_idade = parseInt(document.querySelector('.idade__input').value);
+  const  idadeMinima = 18;
+  const  cond_idade  = in_idade === idadeMinima;
+  const  out_idade   = cond_idade ? 'Apto': 'Inapto';
+  console.log(`IDADE: ${out_idade}`);
+  return out_idade;
+}
